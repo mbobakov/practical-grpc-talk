614a48d9cd1f7ca15dd69c9d6fc8b233a44eafd3
Mstislav Bobakov <m.bobakov@gmail.com>
2018-09-13 15:55:02 +0300

    server impl

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,9 +2,15 @@ package server
 
 import (
 	"context"
+	"net"
+	"time"
+
+	"github.com/mbobakov/practical-grpc-talk/api"
 
 	"github.com/golang/protobuf/ptypes/duration"
 	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
 )
 
 // TS is a time server
@@ -14,11 +20,34 @@ type TS struct{}
 // ServeGRPC interface on the provided address.
 // Blocks until stop or error not occured
 func ServeGRPC(ctx context.Context, l string) error {
-	return nil
+	lis, err := net.Listen("tcp", l)
+	if err != nil {
+		return errors.Wrap(err, "Could't start listen")
+	}
+	s := grpc.NewServer()
+	api.RegisterTimeServer(s, &TS{})
+	errCh := make(chan error, 1)
+	go func() {
+		errCh <- s.Serve(lis)
+	}()
+	select {
+	case err := <-errCh:
+		return errors.Wrap(err, "GRPC server error")
+	case <-ctx.Done():
+		s.GracefulStop()
+		return nil
+	}
 }
 
 // CurrentDayLength returns duration of current day
 // ! works in  the server TimeZone
 func (t *TS) CurrentDayLength(ctx context.Context, e *empty.Empty) (*duration.Duration, error) {
-	panic("NotImpemented!")
+	now := time.Now()
+	year, month, day := now.Date()
+	durFrom := time.Since(time.Date(year, month, day, 0, 0, 0, 0, now.Location()))
+	return &duration.Duration{
+			Seconds: int64(durFrom.Seconds()),
+			Nanos:   int32(durFrom.Nanoseconds() - int64(durFrom.Seconds())*int64(time.Second)),
+		},
+		nil
 }
